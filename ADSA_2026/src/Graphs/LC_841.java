ea3eff120cc30f36bc2eae6dd129779ec9931baf
Rakshit Dhillon <rakshitdhillon@gmail.com>
2026-02-04 14:40:37 +0530

    Changed_LC_841

--- ADSA_2026/src/Graphs/LC_841.java
+++ ADSA_2026/src/Graphs/LC_841.java
@@ -1,4 +1,30 @@
 package Graphs;
 
 public class LC_841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms .size();
+        boolean[] visited = new boolean[n];
+
+        int source =0;
+
+        dfs(source, visited, rooms);
+        for(boolean t: visited){
+            if(!t)
+                return false;
+        }
+        return true;
+
+    }
+    public void dfs(int source, boolean[] visited, List<List<Integer>> rooms){
+
+        visited[source]= true;
+
+        for(int nbr: rooms.get(source)){
+            if(!visited[nbr]){
+                dfs(nbr, visited, rooms);
+
+            }
+        }
+
+    }
 }
